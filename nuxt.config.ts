727111a4357500224df3fc1fe185c77b401d85ff
Rafael Araujo <rflaraujodev@gmail.com>
2023-11-08 16:16:57 -0300

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-  buildModules: ["nuxt-delay-hydration"],
   devtools: { enabled: true },
   css: ["@/assets/styles/custom.sass"],
   ssr: true,
