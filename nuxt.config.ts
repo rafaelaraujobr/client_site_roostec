4e9eb9f9e0043597998fc6b8d51594b152f871aa
Rafael Araujo <rflaraujodev@gmail.com>
2023-11-08 17:08:12 -0300

    pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ["@/assets/styles/custom.sass"],
@@ -7,8 +6,9 @@ export default defineNuxtConfig({
   modules: [
     "nuxt-quasar-ui",
     "@nuxtjs/i18n",
-    "@nuxtjs/robots",
+    // "@nuxtjs/robots",
     "nuxt-delay-hydration",
+    "@vite-pwa/nuxt",
   ],
   delayHydration: {
     mode: "init",
@@ -16,10 +16,10 @@ export default defineNuxtConfig({
   i18n: {
     vueI18n: "./i18n.config.ts",
   },
-  robots: {
-    UserAgent: "*",
-    Disallow: "",
-  },
+  // robots: {
+  //   UserAgent: "*",
+  //   Disallow: "",
+  // },
   quasar: {
     sassVariables: "assets/styles/quasar.variables.sass",
     plugins: [
