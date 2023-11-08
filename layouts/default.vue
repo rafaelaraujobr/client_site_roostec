c9e797fb0623038a4c38ab3cad60dc754ec7a5ca
Rafael Araujo <rflaraujodev@gmail.com>
2023-11-08 14:22:44 -0300

    i18n

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,14 +4,13 @@
       <q-toolbar class="wrapper">
         <q-btn flat dense round icon="sym_r_notes" aria-label="Menu" @click="toggleLeftDrawer" v-if="!$q.screen.gt.sm" />
         <q-toolbar-title> roostec </q-toolbar-title>
-        <q-tabs v-model="tab" no-caps narrow-indicator v-if="$q.screen.gt.sm" dense>
-          <q-route-tab to="/" exact name="home" label="Início" padding="sm md" />
+        <q-tabs v-model="tab" no-caps narrow-indicator v-if="$q.screen.gt.sm" dense class="q-px-md">
+          <q-route-tab to="/" exact name="home" :label="$t('HOME')" />
           <q-route-tab to="/about" exact name="about" label="Sobre nós" />
           <q-route-tab to="/solutions" exact name="solutions" label="Soluções" />
           <q-route-tab to="/articles" exact name="articles" label="Artigos" />
-          <q-route-tab to="/contact" exact name="contact" :label="$t('CONTACT')" padding="sm md" />
+          <q-route-tab to="/contact" exact name="contact" :label="$t('CONTACT')" />
         </q-tabs>
-        <q-separator spaced inset vertical dark v-if="$q.screen.gt.sm" />
         <div class="q-gutter-sm ">
           <change-language />
           <q-btn color="primary" :icon="$q.dark.isActive ? 'sym_r_dark_mode' : 'sym_r_light_mode'" padding="sm"
@@ -28,6 +27,23 @@
     <q-page-container>
       <slot />
     </q-page-container>
+    <q-card class="bg-secondary text-white no-border-radius q-py-lg" flat>
+      <q-card-section class="wrapper q-px-lg" style="padding-bottom: 50px;">
+        <div class="text-h4 q-py-md">Our Changing Planet</div>
+        <div class="text-subtitle2">by John Doe</div>
+        <div class="q-py-sm">
+          Transformando ideias em soluções digitais avançadas. Vamos construir juntos o futuro digital!
+        </div>
+      </q-card-section>
+    </q-card>
+    <q-card flat class="bg-secondary text-white no-border-radius">
+      <q-separator inset dark />
+      <q-card-section class="wrapper">
+        <div class="q-py-xs text-caption text-weight-light text-center">
+          Roostec desenvolvimento LTDA | CNPJ: 52.534.676/0001-69 | Av. das AMERICAS, BARRA DA TIJUCA, 04200 - Rio de de janeiro - RJ - Brasil | +55 21981842373
+        </div>
+      </q-card-section>
+    </q-card>
   </q-layout>
 </template>
 
